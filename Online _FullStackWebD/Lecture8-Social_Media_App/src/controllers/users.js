0fb4807436dc094e4b51ba262d065d1796d69711
Aman Mahajan <aman@Amans-MacBook-Air.local>
2022-03-01 13:26:34 +0530

    random user creation endpoints done

---
+++ Online _FullStackWebD/Lecture8-Social_Media_App/src/controllers/users.js
@@ -0,0 +1,28 @@
+const { Users } = require("../db/model");
+const { genRandomUsername } = require("../utils/username");
+
+async function createAnonUser() {
+  const user = await Users.create({
+    username: genRandomUsername(),
+  });
+  return user;
+}
+
+module.exports = {
+  createAnonUser,
+};
+
+/*  Test Code */
+
+/* async function task() {
+  console.log(await createAnonUser());
+  console.log("---------------------");
+  console.log(await createAnonUser());
+  console.log("---------------------");
+  console.log(await createAnonUser());
+  console.log("---------------------");
+  console.log(await createAnonUser());
+  console.log("---------------------");
+}
+task();
+ */
