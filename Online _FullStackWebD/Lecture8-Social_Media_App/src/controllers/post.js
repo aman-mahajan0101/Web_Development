2beb1dd11e88f884c781bf9acbbdbd4ffcb03dae
Aman Mahajan <amanmahajan987@gmail.com>
2022-03-01 14:46:31 +0530

    controllers post made

---
+++ Online _FullStackWebD/Lecture8-Social_Media_App/src/controllers/post.js
@@ -0,0 +1,47 @@
+const { Posts, Users } = require("../db/model");
+
+async function createNewPost(userId, title, body) {
+  const post = await Posts.create({
+    title,
+    body,
+    userId,
+  });
+  return post;
+}
+
+async function showAllPosts(query) {
+  const posts = await Posts.findAll({
+    include: [Users],
+  });
+  return posts;
+}
+
+module.exports = {
+  createNewPost,
+  showAllPosts,
+};
+
+// async function task() {
+//   console.log(
+//     await createNewPost(
+//       1,
+//       "This is a sample post",
+//       "Body of the post goes here"
+//     )
+//   ),
+//     console.log(
+//       await createNewPost(
+//         2,
+//         "Another sample post",
+//         "Some body example here as well"
+//       )
+//     );
+//   const posts = await showAllPosts();
+//   for (let p of posts) {
+//     console.log(`${p.title}\n${p.body}\n===========\n`);
+//   }
+// }
+
+// task();
+
+// //author:${p.Users.username}
