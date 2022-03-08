2dfc59c7cea1bac618d57754710b5f54ed95c0e5
Aman Mahajan <amanmahajan987@gmail.com>
2022-03-08 22:23:30 +0530

    Reviews getting deleted working

--- Offline_FullStackWebd/Lecture-32-Middlewares and Mongoose relations/E-Commerce-v2.1/routes/productRoutes.js
+++ Offline_FullStackWebd/Lecture-32-Middlewares and Mongoose relations/E-Commerce-v2.1/routes/productRoutes.js
@@ -1,97 +1,93 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Product = require('../models/product');
-const Review = require('../models/review');
-const mongoose = require("mongoose")
+const Product = require("../models/product");
+const Review = require("../models/review");
+const mongoose = require("mongoose");
 
-router.get('/products', async (req, res) => {
-    
-    const products = await Product.find({});
+router.get("/products", async (req, res) => {
+  const products = await Product.find({});
 
-    res.render('products/index', { products });
+  res.render("products/index", { products });
 });
 
-
-router.get('/products/new', (req, res) => {
-    res.render('products/new');
+router.get("/products/new", (req, res) => {
+  res.render("products/new");
 });
 
-router.post('/products', async (req, res) => {
-    const { name, price, desc, img } = req.body;
+router.post("/products", async (req, res) => {
+  const { name, price, desc, img } = req.body;
 
-    await Product.create({ name, price, desc, img });
+  await Product.create({ name, price, desc, img });
 
-    res.redirect('/products');
+  res.redirect("/products");
 });
 
-router.get('/products/:id', async (req, res) => {
-    const { id } = req.params;
+router.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
 
-    const product = await Product.findById(id).populate('reviews');
+  const product = await Product.findById(id).populate("reviews");
 
-    res.render('products/show', { product });
+  res.render("products/show", { product });
 });
 
+router.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
 
-router.get('/products/:id/edit', async (req, res) => {
-    const { id } = req.params;
+  const product = await Product.findById(id);
 
-    const product = await Product.findById(id);
-
-    res.render('products/edit', { product })
+  res.render("products/edit", { product });
 });
 
-router.patch('/products/:id', async (req, res) => {
-    const { id } = req.params;
+router.patch("/products/:id", async (req, res) => {
+  const { id } = req.params;
 
-    const { name, price, img, desc } = req.body;
+  const { name, price, img, desc } = req.body;
 
-    await Product.findByIdAndUpdate(id, { name, price, img, desc });
+  await Product.findByIdAndUpdate(id, { name, price, img, desc });
 
-    res.redirect(`/products/${id}`);
+  res.redirect(`/products/${id}`);
 });
 
-router.delete('/products/:id', async (req, res) => {
-    
-    const { id } = req.params;
+router.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
 
-    await Product.findByIdAndDelete(id);
+  await Product.findByIdAndDelete(id);
 
-    res.redirect('/products');
+  res.redirect("/products");
 });
 
-router.delete('/products/:productid/:reviewid', async (req, res) => {
-    
-    const { productid,reviewid } = req.params;
+router.delete("/products/:productid/:reviewid", async (req, res) => {
+  const { productid, reviewid } = req.params;
 
-    await Review.findByIdAndDelete(reviewid);
+  await Review.findByIdAndDelete(reviewid);
 
-    res.redirect(`/products/${productid}`);
+  const product = await Product.findById(productid);
+  console.log(product.reviews);
+
+  res.redirect(`/products/${productid}`);
 });
 
-router.post('/products/:productid/review', async(req, res) => {
-    
-    const { productid } = req.params;
-    const { rating, comment } = req.body;
+router.post("/products/:productid/review", async (req, res) => {
+  const { productid } = req.params;
+  const { rating, comment } = req.body;
 
-    // finding the product to add review
-    const product = await Product.findById(productid);
+  // finding the product to add review
+  const product = await Product.findById(productid);
 
-    // creating a new review with given rating and comment
-    const review = new Review({ rating, comment });
+  // creating a new review with given rating and comment
+  const review = new Review({ rating, comment });
 
-    // adding a review to reviews array of a product
-    product.reviews.push(review);
-    
-    // saving the review in a database
+  // adding a review to reviews array of a product
+  product.reviews.push(review);
 
-    await review.save();
+  // saving the review in a database
 
-    // saving the product in a database
-    await product.save();
+  await review.save();
 
+  // saving the product in a database
+  await product.save();
 
-    res.redirect(`/products/${productid}`);
-})
+  res.redirect(`/products/${productid}`);
+});
 
 module.exports = router;
