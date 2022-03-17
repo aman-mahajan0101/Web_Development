253a30fbda208a46e29e6c75839ccc72e21200b2
Aman Mahajan <amanmahajan987@gmail.com>
2022-03-18 00:59:17 +0530

    Average Rating working except deletion case

--- Offline_FullStackWebd/Lecture-36 /E-Commerce-v5.1/models/product.js
+++ Offline_FullStackWebd/Lecture-36 /E-Commerce-v5.1/models/product.js
@@ -1,35 +1,32 @@
-const mongoose = require('mongoose');
-const Review = require('./review');
-
-
+const mongoose = require("mongoose");
+const Review = require("./review");
 
 const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    img: String,
-    desc: String,
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'User'
+  name: String,
+  price: Number,
+  img: String,
+  desc: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  avgRating: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
     },
-    reviews: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref:'Review'
-        }
-    ]
+  ],
 });
-
-
 
 // Mongoose Middlware to delete reviews corresponding to particular product
-productSchema.post('findOneAndDelete', async function (product) {
-    await Review.deleteMany({ _id: { $in: product.reviews } });
+productSchema.post("findOneAndDelete", async function (product) {
+  await Review.deleteMany({ _id: { $in: product.reviews } });
 });
 
-
-
-const Product = mongoose.model('Product', productSchema);
-
+const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
