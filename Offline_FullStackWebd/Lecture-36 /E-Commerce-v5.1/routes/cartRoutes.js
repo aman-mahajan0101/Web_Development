3205ee912262159e88732ff9206a2f3e2840583c
Aman Mahajan <amanmahajan987@gmail.com>
2022-03-18 10:52:31 +0530

    Cart delete button working but functionality is not working

--- Offline_FullStackWebd/Lecture-36 /E-Commerce-v5.1/routes/cartRoutes.js
+++ Offline_FullStackWebd/Lecture-36 /E-Commerce-v5.1/routes/cartRoutes.js
@@ -1,51 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { isLoggedIn } = require('../middleware');
-const User = require('../models/user');
+const { isLoggedIn } = require("../middleware");
+const User = require("../models/user");
 
+router.get("/user/cart", isLoggedIn, async (req, res) => {
+  const userid = req.user._id;
+  const user = await User.findById(userid).populate("cart.item");
 
+  const totalAmount = user.cart.reduce((currentSum, product) => currentSum + product.item.price * product.qty, 0);
 
-router.get('/user/cart',isLoggedIn, async (req, res) => {
-
-    const userid = req.user._id;
-    const user = await User.findById(userid).populate('cart.item');
-
-    const totalAmount = user.cart.reduce((currentSum,product) => currentSum + product.item.price * product.qty, 0);    
-
-
-    res.render('cart/cart',{user,totalAmount});
+  res.render("cart/cart", { user, totalAmount });
 });
 
+router.post("/user/:productid/cart", isLoggedIn, async (req, res) => {
+  const { productid } = req.params;
+  const userid = req.user._id;
 
+  const user = await User.findById(userid);
 
-router.post('/user/:productid/cart', isLoggedIn, async (req, res) => {
-   
+  const isPresent = user.cart.some((product) => product.item.equals(productid));
+
+  if (isPresent) {
+    const newCartArray = user.cart.map((product) => {
+      return product.item.equals(productid) ? { item: product.item, qty: product.qty + 1 } : product;
+    });
+    user.cart.splice(0, user.cart.length);
+    user.cart.push(...newCartArray);
+    await user.save();
+  } else {
+    user.cart.push({ item: productid });
+    await user.save();
+  }
+
+  res.redirect("/user/cart");
+});
+
+router.delete("/user/:productid/cart", isLoggedIn, async (req, res) => {
+  try {
     const { productid } = req.params;
     const userid = req.user._id;
+    // console.log(userid);
+    console.log(productid);
 
     const user = await User.findById(userid);
+    console.log(user);
+    await user.findByIdAndUpdate(user._id, { $pull: { cart: productid } });
 
-    const isPresent = user.cart.some((product) => product.item.equals(productid));
-
-    if (isPresent) {
-        const newCartArray = user.cart.map((product) => {
-            return product.item.equals(productid) ? { item:product.item, qty: product.qty + 1 } : product;
-        });
-        user.cart.splice(0, user.cart.length);
-        user.cart.push(...newCartArray);
-        await user.save();
-    } else {
-        user.cart.push({ item: productid });
-        await user.save();
-    }
-    
-    res.redirect('/user/cart');
+    res.redirect(`/user/cart`);
+  } catch (e) {
+    console.error(e);
+    req.flash("error", e.message);
+    res.redirect("/error");
+  }
 });
-
-
-
-
-
-
 
 module.exports = router;
