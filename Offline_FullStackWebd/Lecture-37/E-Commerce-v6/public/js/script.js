839076da083788475d426c8d71bb7a989fe6d616
Aman Mahajan <amanmahajan987@gmail.com>
2022-03-20 02:58:31 +0530

    Like functionality not working properly

--- Offline_FullStackWebd/Lecture-37/E-Commerce-v6/public/js/script.js
+++ Offline_FullStackWebd/Lecture-37/E-Commerce-v6/public/js/script.js
@@ -1,12 +1,17 @@
-const productContainer = document.querySelector('.products');
+const productContainer = document.querySelector(".products");
 
+productContainer.addEventListener("click", async (e) => {
+  if (e.target.nodeName === "I") {
+    const id = e.target.getAttribute("product-id");
+    const res = await axios.post(`/user/${id}/like`);
+    console.log(res);
 
-
-productContainer.addEventListener('click', async(e) => {
-    if (e.target.nodeName === "I") {
-        const id = e.target.getAttribute('product-id');
-        const res = await axios.post(`/user/${id}/like`);
-        console.log(res);
+    if (e.target.classList.contains("fas")) {
+      e.target.classList.remove("fas");
+      e.target.classList.add("far");
+    } else {
+      e.target.classList.add("fas");
+      e.target.classList.remove("far");
     }
+  }
 });
-
--- Offline_FullStackWebd/Lecture-37/E-Commerce-v6/routes/apis/likeProduct.js
+++ Offline_FullStackWebd/Lecture-37/E-Commerce-v6/routes/apis/likeProduct.js
@@ -1,19 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { isLoggedIn } = require('../../middleware');
-const User = require('../../models/user');
+const { isLoggedIn } = require("../../middleware");
+const User = require("../../models/user");
 
+router.post("/user/:productid/like", isLoggedIn, async (req, res) => {
+  const { productid } = req.params;
 
+  const user = req.user;
+  const isLiked = user.wishList.includes(productid);
 
+  if (isLiked) {
+    req.user = await User.findByIdAndUpdate(req.user._id, { $pull: { wishList: productid } });
+  } else {
+    req.user = await User.findByIdAndUpdate(req.user._id, { $addToSet: { wishList: productid } });
+  }
 
-
-router.post('/user/:productid/like', async (req, res) => {
-    
-    const { productid } = req.params;
-
-    res.status(200).json({ id: productid, message: 'Liked the product Successfully' });
-})
-
-
+  res.status(200).json({ id: productid, message: "Liked the product Successfully" });
+});
 
 module.exports = router;
