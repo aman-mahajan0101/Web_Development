d83079562cf938792621d298f0c9f6a215db4fb2
Aman Mahajan <amanmahajan987@gmail.com>
2022-03-20 02:40:00 +0530

    total amount is not working with axios

--- Offline_FullStackWebd/Lecture-36 /E-Commerce-v5.1/public/js/cart.js
+++ Offline_FullStackWebd/Lecture-36 /E-Commerce-v5.1/public/js/cart.js
@@ -1,24 +1,58 @@
-const cartContainer = document.querySelector(".quantity-container");
+// const cartContainer = document.querySelector(".quantity-container");
+// const cartContainer = document.querySelector(".container");
+// const cartContainer = document.querySelector(".col-8");
+const cartContainer = document.querySelector(".card-body");
 const quantity = document.querySelector(".quantity");
+const amtquantity = document.querySelector(".amt-quantity");
+const totalamt = document.querySelector(".list-group-item");
 
-console.log(quantity);
-//quantity null aa rhi h as ejs script ke through le rha ho input but vo ejs ke through accept kaise krega idk
+// console.log(cartContainer);
+// console.log(quantity);
+// console.log(amtquantity);
+console.log(totalamt);
 
 cartContainer.addEventListener("click", async (e) => {
   const tar = e.target;
+
   if (tar.classList.contains("fa-minus")) {
     console.log("minus clicked");
     const id = e.target.getAttribute("product-id");
-    const val = quantity.innerText;
-    console.log(val);
-    quantity.innerText = val - 1;
+
+    //For changing the input of inc and dec span
+    let val = quantity.innerText;
+    val = Number(val) - 1;
+    quantity.innerText = val;
+
+    //for changing the input of total price
+    let amtval = amtquantity.innerText;
+    amtval = Number(amtval) - 1;
+    amtquantity.innerText = amtval;
+
+    //for calculating total amount
+    const price = e.target.getAttribute("price");
+    console.log(price);
+
+    totalamt.innerText = Number(price) * Number(amtval);
+
     const res = await axios.post(`/user/${id}/dec/cart`);
   } else if (tar.classList.contains("fa-plus")) {
     console.log("plus clicked");
     const id = e.target.getAttribute("product-id");
-    const val = quantity.innerText;
-    console.log(val);
-    quantity.innerText = val - 1;
+    //For changing the input of inc and dec span
+    let val = quantity.innerText;
+    val = Number(val) + 1;
+    quantity.innerText = val;
+
+    //for changing the input of total price
+    let amtval = amtquantity.innerText;
+    amtval = Number(amtval) + 1;
+    amtquantity.innerText = amtval;
+
+    //for calculating total amount
+    const price = e.target.getAttribute("price");
+    console.log(price);
+    totalamt.innerText = Number(price) * Number(amtval);
+
     const res = await axios.post(`/user/${id}/inc/cart`);
   }
 });
