eb45873b6eae7810fdf8f940900d7e1495f3f0ba
Aman Mahajan <amanmahajan987@gmail.com>
2022-03-24 19:26:13 +0530

    Inc Dec for each cart item working

--- Offline_FullStackWebd/Lecture-36 /E-Commerce-v5.1/public/js/cart.js
+++ Offline_FullStackWebd/Lecture-36 /E-Commerce-v5.1/public/js/cart.js
@@ -1,35 +1,36 @@
-// const cartContainer = document.querySelector(".quantity-container");
-// const cartContainer = document.querySelector(".container");
-// const cartContainer = document.querySelector(".col-8");
 const cartContainer = document.querySelector(".cart");
 const quantity = document.querySelector(".quantity");
 const amtquantity = document.querySelector(".amt-quantity");
 const totalamt = document.querySelector(".list-group-item");
 
-// console.log(cartContainer);
-// console.log(quantity);
-// console.log(amtquantity);
-console.log(totalamt);
-
-// cartContainer.addEventListener("click", (e) => {
-//   console.log(e.target);
-// });
+// console.log(totalamt);
 
 cartContainer.addEventListener("click", async (e) => {
   const tar = e.target;
+
+  console.log(tar);
 
   if (tar.classList.contains("fa-minus")) {
     console.log("minus clicked");
     const id = e.target.getAttribute("product-id");
 
-    console.log(id);
+    //For changing the input of inc and dec span
+    tar.parentElement.nextElementSibling.innerText = Number(tar.parentElement.nextElementSibling.innerText) - 1;
+
+    const res = await axios.post(`/user/${id}/dec/cart`);
+  } else if (tar.classList.contains("fa-plus")) {
+    console.log("plus clicked");
+    const id = e.target.getAttribute("product-id");
 
     //For changing the input of inc and dec span
-    let val = quantity.innerText;
-    val = Number(val) - 1;
-    quantity.innerText = val;
+    tar.parentElement.previousElementSibling.innerText = Number(tar.parentElement.previousElementSibling.innerText) + 1;
 
-    //for changing the input of total price
+    const res = await axios.post(`/user/${id}/inc/cart`);
+  }
+});
+
+/*
+//for changing the input of total price
     let amtval = amtquantity.innerText;
     amtval = Number(amtval) - 1;
     amtquantity.innerText = amtval;
@@ -40,16 +41,11 @@ cartContainer.addEventListener("click", async (e) => {
 
     totalamt.innerText = Number(price) * Number(amtval);
 
-    const res = await axios.post(`/user/${id}/dec/cart`);
-  } else if (tar.classList.contains("fa-plus")) {
-    console.log("plus clicked");
-    const id = e.target.getAttribute("product-id");
-    //For changing the input of inc and dec span
-    let val = quantity.innerText;
-    val = Number(val) + 1;
-    quantity.innerText = val;
 
-    //for changing the input of total price
+    ...
+
+
+     //for changing the input of total price
     let amtval = amtquantity.innerText;
     amtval = Number(amtval) + 1;
     amtquantity.innerText = amtval;
@@ -59,6 +55,6 @@ cartContainer.addEventListener("click", async (e) => {
     console.log(price);
     totalamt.innerText = Number(price) * Number(amtval);
 
-    const res = await axios.post(`/user/${id}/inc/cart`);
-  }
-});
+
+    <button class="text-danger" type="submit"><i class="fas fa-trash fa-lg"></i></button>
+*/
