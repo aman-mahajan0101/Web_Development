eb04a75c62e41d84f5176e4773cfe047fa4cceee
Aman Mahajan <amanmahajan987@gmail.com>
2022-03-25 20:11:18 +0530

    Total Amount changing using DOM

--- Offline_FullStackWebd/Lecture-37/E-Commerce-v6.2/public/js/cart.js
+++ Offline_FullStackWebd/Lecture-37/E-Commerce-v6.2/public/js/cart.js
@@ -1,61 +1,63 @@
 const cartContainer = document.querySelector(".cart");
-const quantity = document.querySelector(".quantity");
-const amtquantity = document.querySelector(".amt-quantity");
+const ul = document.querySelector(".list-group");
+var items = ul.getElementsByTagName("li");
+var total = ul.lastElementChild;
 
 cartContainer.addEventListener("click", async (e) => {
   const tar = e.target;
+  const id = e.target.getAttribute("product-id");
+  const name = e.target.getAttribute("product-name");
+  const price = e.target.getAttribute("product-price");
 
   if (tar.classList.contains("fa-minus")) {
-    const id = e.target.getAttribute("product-id");
-
     if (Number(tar.parentElement.nextElementSibling.innerText) === 1) {
       tar.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "none";
+      // items[i].children[0].style.display = "none";
+      for (var i = 0; i < items.length - 1; i++) {
+        if (items[i].children[0].outerText.includes(name)) {
+          console.log(items[i]);
+          items[i].style.display = "none";
+
+          total.children[0].innerText = Number(total.children[0].innerText) - Number(price);
+        }
+      }
+
       const res = await axios.delete(`/user/${id}/cart`);
     } else {
       tar.parentElement.nextElementSibling.innerText = Number(tar.parentElement.nextElementSibling.innerText) - 1;
+
+      for (var i = 0; i < items.length - 1; i++) {
+        if (items[i].children[0].outerText.includes(name)) {
+          let quan = Number(items[i].children[1].innerText);
+
+          quan -= 1;
+
+          items[i].children[1].innerText = quan;
+
+          total.children[0].innerText = Number(total.children[0].innerText) - Number(price);
+        }
+      }
+
       const res = await axios.post(`/user/${id}/dec/cart`);
     }
   } else if (tar.classList.contains("fa-plus")) {
-    const id = e.target.getAttribute("product-id");
-
     //For changing the input of inc and dec span
     tar.parentElement.previousElementSibling.innerText = Number(tar.parentElement.previousElementSibling.innerText) + 1;
+
+    //For total amount change and quantity change
+
+    for (var i = 0; i < items.length - 1; i++) {
+      if (items[i].children[0].outerText.includes(name)) {
+        let quan = Number(items[i].children[1].innerText);
+
+        quan += 1;
+
+        items[i].children[1].innerText = quan;
+
+        total.children[0].innerText = Number(total.children[0].innerText) + Number(price);
+      }
+    }
 
     const res = await axios.post(`/user/${id}/inc/cart`);
   }
 });
-
-/*
-//for changing the input of total price
-    let amtval = amtquantity.innerText;
-    amtval = Number(amtval) - 1;
-    amtquantity.innerText = amtval;
-
-    //for calculating total amount
-    const price = e.target.getAttribute("price");
-    console.log(price);
-
-    totalamt.innerText = Number(price) * Number(amtval);
-
-
-    ...
-
-
-     //for changing the input of total price
-    let amtval = amtquantity.innerText;
-    amtval = Number(amtval) + 1;
-    amtquantity.innerText = amtval;
-
-    //for calculating total amount
-    const price = e.target.getAttribute("price");
-    console.log(price);
-    totalamt.innerText = Number(price) * Number(amtval);
-
-
-    <button class="text-danger" type="submit"><i class="fas fa-trash fa-lg"></i></button>
-const a = document.querySelector(".list-group")
-    a.lastElementChild.innerText=
-
-    or(int i=0;i<children.length;i++){if(children[0].children[0].iinnerText==="Iphone 11 -"){a.lastElementChild.innerText="hi"}}
-    var children = [].slice.call(a.children);
-*/
