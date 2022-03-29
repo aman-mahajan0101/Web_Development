344da2fba38083d20adc9adf04af942768218890
Aman Mahajan <amanmahajan987@gmail.com>
2022-03-29 22:15:59 +0530

    Icon change for cart dec. added

--- Offline_FullStackWebd/Lecture-37/E-Commerce-v6.2/public/js/cart.js
+++ Offline_FullStackWebd/Lecture-37/E-Commerce-v6.2/public/js/cart.js
@@ -12,7 +12,7 @@ cartContainer.addEventListener("click", async (e) => {
   if (tar.classList.contains("fa-minus")) {
     if (Number(tar.parentElement.nextElementSibling.innerText) === 1) {
       tar.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "none";
-      // items[i].children[0].style.display = "none";
+
       for (var i = 0; i < items.length - 1; i++) {
         if (items[i].children[0].outerText.includes(name)) {
           console.log(items[i]);
@@ -25,6 +25,12 @@ cartContainer.addEventListener("click", async (e) => {
       const res = await axios.delete(`/user/${id}/cart`);
     } else {
       tar.parentElement.nextElementSibling.innerText = Number(tar.parentElement.nextElementSibling.innerText) - 1;
+      console.log("Clicked");
+      if (tar.parentElement.nextElementSibling.innerText === "1") {
+        console.log(tar);
+        // tar.classList.remove("fa-minus");
+        tar.classList.add("fa-trash");
+      }
 
       for (var i = 0; i < items.length - 1; i++) {
         if (items[i].children[0].outerText.includes(name)) {
@@ -44,8 +50,11 @@ cartContainer.addEventListener("click", async (e) => {
     //For changing the input of inc and dec span
     tar.parentElement.previousElementSibling.innerText = Number(tar.parentElement.previousElementSibling.innerText) + 1;
 
+    if (Number(tar.parentElement.previousElementSibling.innerText) >= 2) {
+      console.log(tar.parentElement.previousElementSibling.previousElementSibling.children[0]);
+      tar.parentElement.previousElementSibling.previousElementSibling.children[0].classList.remove("fa-trash");
+    }
     //For total amount change and quantity change
-
     for (var i = 0; i < items.length - 1; i++) {
       if (items[i].children[0].outerText.includes(name)) {
         let quan = Number(items[i].children[1].innerText);
