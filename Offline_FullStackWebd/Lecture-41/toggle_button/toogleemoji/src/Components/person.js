324992b515fea159784adca5ce157d887024333e
Aman Mahajan <amanmahajan987@gmail.com>
2022-04-02 23:11:33 +0530

    toggle emoji

---
+++ Offline_FullStackWebd/Lecture-41/toggle_button/toogleemoji/src/Components/person.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import useToggle from "../hooks/useToggle";
+
+const Person = () => {
+  const [toggle, setToggle] = useToggle(true);
+
+  return (
+    <div style={{ width: "360px", margin: "30px auto", border: "2px solid #ccc" }}>
+      <h2 onClick={setToggle}>{toggle ? "😄" : "😢"}</h2>
+    </div>
+  );
+};
+
+export default Person;
