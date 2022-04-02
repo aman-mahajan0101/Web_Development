b807c3a7ce0024fbb9992162d5f840d237953ed1
Aman Mahajan <amanmahajan987@gmail.com>
2022-04-02 11:48:21 +0530

    Filter using ajax working

--- Offline_FullStackWebd/Lecture-37/E-Commerce-v6.5/public/js/filter.js
+++ Offline_FullStackWebd/Lecture-37/E-Commerce-v6.5/public/js/filter.js
@@ -2,4 +2,5 @@ async function filter() {
   var value = document.getElementById("filter").value;
   console.log(value);
   const res = await axios.get(`/products/filtered/${value}`);
+  window.location.replace(`/products/filtered/${value}`);
 }
