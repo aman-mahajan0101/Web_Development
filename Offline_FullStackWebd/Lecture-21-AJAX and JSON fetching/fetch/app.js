797c86af5d2408c7daa61028133f6c242d2cb40f
Aman Mahajan <amanmahajan987@gmail.com>
2022-04-06 18:40:37 +0530

    minor change

--- Offline_FullStackWebd/Lecture-21-AJAX and JSON fetching/fetch/app.js
+++ Offline_FullStackWebd/Lecture-21-AJAX and JSON fetching/fetch/app.js
@@ -1,13 +1,11 @@
-
-
-fetch('https://api.cryptonator.com/api/ticker/btc-usd')
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data.ticker.price);
-    })
-    .catch((err) => {
-        console.log('Inside Catch')
-        console.log(err.message);
-    });
+fetch("https://api.cryptonator.com/api/ticker/btc-usd")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data.ticker.price);
+  })
+  .catch((err) => {
+    console.log("Inside Catch");
+    console.log(err.message);
+  });
