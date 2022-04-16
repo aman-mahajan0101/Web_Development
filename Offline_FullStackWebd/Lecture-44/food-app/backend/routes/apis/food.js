2c4dad605524ab96e32cf214b28f97d0121e478b
Aman Mahajan <amanmahajan987@gmail.com>
2022-04-16 12:15:31 +0530

    Few more css properties dded

--- Offline_FullStackWebd/Lecture-44/food-app/backend/routes/apis/food.js
+++ Offline_FullStackWebd/Lecture-44/food-app/backend/routes/apis/food.js
@@ -1,13 +1,9 @@
-const router = require('express').Router();
-const Food = require('../../models/food');
+const router = require("express").Router();
+const Food = require("../../models/food");
 
-
-
-
-router.get('/allfoods', async (req, res) => {
-    const foods = await Food.find({});
-    res.json(foods);
+router.get("/allfoods", async (req, res) => {
+  const foods = await Food.find({});
+  res.json(foods);
 });
-
 
 module.exports = router;
